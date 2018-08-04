27cf95aa248ce42ec2f3903334c8ede05ab0d41f
zcy <zurl@live.com>
2018-08-04 22:57:26 +0800

    "0.1.2"

--- ide/js/main.js
+++ ide/js/main.js
@@ -2,7 +2,7 @@ import * as Poly from 'babel-polyfill';
 import * as Ace from "./ace/ace";
 import * as AceMonokai from "./ace/theme-monokai"
 import * as AceCCpp from "./ace/mode-c_cpp";
-import {StringInputFile} from "../../src/runtime/vmfile";
+import {version} from "./version";
 
 const messageDiv = document.getElementById("message");
 const outputDiv = document.getElementById("output");
@@ -12,14 +12,26 @@ editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/c_cpp");
 
 const tabdiv = M.Tabs.init(document.getElementById("tab-div"), {});
-document.getElementById("version").innerText = "v0.1.0";
+document.getElementById("version").innerText = "v" + version;
 
 let obj = null;
 
 function showMessage(type, message){
+    tabdiv.select("message");
     const line = document.createElement("div");
     line.innerText = `[${type}] : ${message}`;
     messageDiv.appendChild(line);
+    messageDiv.scrollTop = 1000000;
+}
+
+function showError(error){
+    tabdiv.select("message");
+    const line = document.createElement("div");
+    line.innerText = error.toString();
+    messageDiv.appendChild(line);
+    const line2 = document.createElement("div");
+    line2.innerText = ">> " + error.errorLine;
+    messageDiv.appendChild(line2);
     messageDiv.scrollTop = 1000000;
 }
 
@@ -27,11 +39,15 @@ function showOutput(message){
     outputDiv.innerText += message;
 }
 
+let isFirst = true;
 async function run() {
     outputDiv.innerText = "";
-    tabdiv.select("message");
+    if(isFirst){
+        showMessage("compiler", "downloading compiler");
+        isFirst = false;
+    }
+    const {NativeRuntime, importObj,StringInputFile, compileFile,CompilerError, CallbackOutputFile, NoInputFile} = await import("../../src/tools/api_tools");
     showMessage("compiler", "cc -o main main.cpp");
-    const {NativeRuntime, importObj,StringInputFile, compileFile, CallbackOutputFile, NoInputFile} = await import("../../src/tools/api_tools");
     try {
         const obj = compileFile("main.cpp", editor.getValue());
         if (obj == null) {
@@ -46,10 +62,11 @@ async function run() {
         ]);
         tabdiv.select("output");
         await runtime.run();
-        showMessage("runtime", "function execution finished");
+        showMessage("runtime", "code return with code 0");
+        tabdiv.select("output");
     }catch(e){
-        tabdiv.select("message");
-        showMessage("error", e.toString());
+        if( e instanceof CompilerError ) showError(e);
+        else showMessage("error", e.toString());
     }
 }
 
---
+++ src/wasm/computer.ts
@@ -0,0 +1,5 @@
+/**
+ *  @file
+ *  @author zcy <zurl@live.com>
+ *  Created at 14/07/2018
+ */
